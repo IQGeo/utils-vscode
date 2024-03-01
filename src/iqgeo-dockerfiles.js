a9d63e59295c21e890762ce344ae2616af9a6005
Luis Camacho <luis.camacho@iqgeo.com>
2024-03-01 22:58:41 +0000

    improve handling of missing files

--- src/iqgeo-dockerfiles.js
+++ src/iqgeo-dockerfiles.js
@@ -8,6 +8,122 @@ const aptGetMappings = {
     saml: ['libxml2-dev', 'libxmlsec1-dev'],
 };
 
+const fileModifications = {
+    '.devcontainer/dockerfile': (config, content) => {
+        const { modules, platform } = config;
+
+        content = replaceModuleInjection(content, modules, true);
+
+        content = content.replace(/platform-devenv:.*/, `platform-devenv:${platform.version}`);
+        return content;
+    },
+
+    '.devcontainer/docker-compose.yml': (config, content) => {
+        const { modules, prefix, db_name } = config;
+        const localModules = modules.filter((def) => !def.version || def.devSrc);
+        const newContent = localModules
+            .map(
+                ({ name, devSrc }) =>
+                    `            - ../${devSrc}:/opt/iqgeo/platform/WebApps/myworldapp/modules/${name}:delegated`
+            )
+            .join('\n');
+        content = content.replace(
+            /(# START SECTION.*)[\s\S]*?(.*# END SECTION)/,
+            `$1\n${newContent}\n$2`
+        );
+        content = content.replace(/\${PROJ_PREFIX:-myproj}/g, `\${PROJ_PREFIX:-${prefix}}`);
+        content = content.replace(/\${MYW_DB_NAME:-iqgeo}/g, `\${MYW_DB_NAME:-${db_name}}`);
+        content = content.replace(/iqgeo_devserver:/, `iqgeo_${prefix}_devserver:`);
+        return content;
+    },
+
+    '.devcontainer/.env.example': (config, content) => {
+        const { prefix, db_name } = config;
+        content = content.replace(`PROJ_PREFIX=myproj`, `PROJ_PREFIX=${prefix}`);
+        content = content.replace(
+            `COMPOSE_PROJECT_NAME=myproj_dev\n`,
+            `COMPOSE_PROJECT_NAME=${prefix}_dev\n`
+        );
+        content = content.replace(`MYW_DB_NAME=dev_db\n`, `MYW_DB_NAME=${db_name}\n`);
+        return content;
+    },
+
+    '.devcontainer/devcontainer.json': (config, content) => {
+        const { prefix, display_name } = config;
+        content = content.replace(
+            `"name": "IQGeo Module Development Template"`,
+            `"name": "${display_name}"`
+        );
+        content = content.replace(
+            `"service": "iqgeo_devserver"`,
+            `"service": "iqgeo_${prefix}_devserver"`
+        );
+        return content;
+    },
+
+    'deployment/dockerfile.build': (config, content) => {
+        const { modules, platform } = config;
+        content = replaceModuleInjection(content, modules);
+        content = content.replace(/platform-build:\S+/g, `platform-build:${platform.version}`);
+        return content;
+    },
+
+    'deployment/dockerfile.appserver': (config, content) => {
+        const { modules, platform } = config;
+
+        let aptGets = platform.appserver
+            .map((name) => aptGetMappings[name] ?? [])
+            .flat()
+            .join(' ');
+        const section1 = aptGets
+            ? `RUN apt-get update && \\\n    apt-get install -y ${aptGets} \\\n    && apt-get autoremove && apt-get clean\n\n` +
+              `RUN myw_product fetch pip_packages --include ${platform.appserver.join(' ')}`
+            : '';
+
+        content = content.replace(
+            /(# START SECTION optional dependencies.*)[\s\S]*?(# END SECTION)/,
+            `$1\n${section1}\n$2`
+        );
+
+        content = content.replace(
+            /platform-appserver:\S+/g,
+            `platform-appserver:${platform.version}`
+        );
+
+        const section2 = modules
+            .filter(({ devOnly }) => !devOnly)
+            .map(({ name }) =>
+                ['__init__.py', 'version_info.json', 'server/', 'public/']
+                    .map(
+                        (path) =>
+                            `COPY --chown=www-data:www-data --from=iqgeo_builder \${MODULES}/${name}/${path} \${MODULES}/${name}/${
+                                path.endsWith('/') ? path : ''
+                            }`
+                    )
+                    .join('\n')
+            )
+            .join('\n');
+        content = content.replace(
+            /(# START SECTION Copy modules.*)[\s\S]*?(# END SECTION)/,
+            `$1\n${section2}\n$2`
+        );
+        return content;
+    },
+
+    'deployment/dockerfile.tools': (config, content) => {
+        const { platform } = config;
+        content = content.replace(/platform-tools:\S+/g, `platform-tools:${platform.version}`);
+        return content;
+    },
+
+    'deployment/.env.example': (config, content) => {
+        const { prefix, db_name } = config;
+        content = content.replace(`PROJ_PREFIX=myproj\n`, `PROJ_PREFIX=${prefix}\n`);
+        content = content.replace(`MYW_DB_NAME=myproj\n`, `MYW_DB_NAME=${db_name}\n`);
+        return content;
+    },
+};
+
 /**
  *
  */
@@ -29,14 +145,15 @@ class IQGeoDockerfiles {
             return;
         }
         try {
-            this._updateFiles(root, config);
+            const allUpdated = this._updateFiles(root, config);
+            if (allUpdated)
+                vscode.window.showInformationMessage('IQGeo project configured successfully!');
+            else vscode.window.showWarningMessage('IQGeo project configured with warnings');
         } catch (e) {
             vscode.window.showErrorMessage('Failed to update files');
             console.error(e);
             return;
         }
-
-        vscode.window.showInformationMessage('IQGeo project configured successfully!');
     }
 
     _readConfig(root) {
@@ -55,126 +172,30 @@ class IQGeoDockerfiles {
     _updateFiles(root, config) {
         const update = fileUpdater(root, config);
 
-        update('.devcontainer/dockerfile', (config, content) => {
-            const { modules, platform } = config;
-
-            content = replaceModuleInjection(content, modules, true);
-
-            content = content.replace(/platform-devenv:.*/, `platform-devenv:${platform.version}`);
-            return content;
+        const errors = Object.entries(fileModifications).map(([relPath, transform]) => {
+            try {
+                return update(relPath, transform);
+            } catch (e) {
+                console.warn(`Failed to update file ${relPath}`);
+                console.error(e);
+                return e;
+            }
         });
+        return !errors.some((e) => e);
 
-        update('.devcontainer/docker-compose.yml', (config, content) => {
-            const { modules, prefix, db_name } = config;
-            const localModules = modules.filter((def) => !def.version || def.devSrc);
-            const newContent = localModules
-                .map(
-                    ({ name, devSrc }) =>
-                        `            - ../${devSrc}:/opt/iqgeo/platform/WebApps/myworldapp/modules/${name}:delegated`
-                )
-                .join('\n');
-            content = content.replace(
-                /(# START SECTION.*)[\s\S]*?(.*# END SECTION)/,
-                `$1\n${newContent}\n$2`
-            );
-            content = content.replace(/\${PROJ_PREFIX:-myproj}/g, `\${PROJ_PREFIX:-${prefix}}`);
-            content = content.replace(/\${MYW_DB_NAME:-iqgeo}/g, `\${MYW_DB_NAME:-${db_name}}`);
-            content = content.replace(/iqgeo_devserver:/, `iqgeo_${prefix}_devserver:`);
-            return content;
-        });
-
-        update('.devcontainer/.env.example', (config, content) => {
-            const { prefix, db_name } = config;
-            content = content.replace(`PROJ_PREFIX=myproj`, `PROJ_PREFIX=${prefix}`);
-            content = content.replace(
-                `COMPOSE_PROJECT_NAME=myproj_dev\n`,
-                `COMPOSE_PROJECT_NAME=${prefix}_dev\n`
-            );
-            content = content.replace(`MYW_DB_NAME=dev_db\n`, `MYW_DB_NAME=${db_name}\n`);
-            return content;
-        });
-
-        update('.devcontainer/devcontainer.json', (config, content) => {
-            const { prefix, display_name } = config;
-            content = content.replace(
-                `"name": "IQGeo Module Development Template"`,
-                `"name": "${display_name}"`
-            );
-            content = content.replace(
-                `"service": "iqgeo_devserver"`,
-                `"service": "iqgeo_${prefix}_devserver"`
-            );
-            return content;
-        });
-
-        update('deployment/dockerfile.build', (config, content) => {
-            const { modules, platform } = config;
-            content = replaceModuleInjection(content, modules);
-            content = content.replace(/platform-build:\S+/g, `platform-build:${platform.version}`);
-            return content;
-        });
-
-        update('deployment/dockerfile.appserver', (config, content) => {
-            const { modules, platform } = config;
-
-            let aptGets = platform.appserver
-                .map((name) => aptGetMappings[name] ?? [])
-                .flat()
-                .join(' ');
-            const section1 = aptGets
-                ? `RUN apt-get update && \\\n    apt-get install -y ${aptGets} \\\n    && apt-get autoremove && apt-get clean\n\n` +
-                  `RUN myw_product fetch pip_packages --include ${platform.appserver.join(' ')}`
-                : '';
-
-            content = content.replace(
-                /(# START SECTION optional dependencies.*)[\s\S]*?(# END SECTION)/,
-                `$1\n${section1}\n$2`
-            );
-
-            content = content.replace(
-                /platform-appserver:\S+/g,
-                `platform-appserver:${platform.version}`
-            );
-
-            const section2 = modules
-                .filter(({ devOnly }) => !devOnly)
-                .map(({ name }) =>
-                    ['__init__.py', 'version_info.json', 'server/', 'public/']
-                        .map(
-                            (path) =>
-                                `COPY --chown=www-data:www-data --from=iqgeo_builder \${MODULES}/${name}/${path} \${MODULES}/${name}/${
-                                    path.endsWith('/') ? path : ''
-                                }`
-                        )
-                        .join('\n')
-                )
-                .join('\n');
-            content = content.replace(
-                /(# START SECTION Copy modules.*)[\s\S]*?(# END SECTION)/,
-                `$1\n${section2}\n$2`
-            );
-            return content;
-        });
-
-        update('deployment/dockerfile.tools', (config, content) => {
-            const { platform } = config;
-            content = content.replace(/platform-tools:\S+/g, `platform-tools:${platform.version}`);
-            return content;
-        });
-
-        update('deployment/.env.example', (config, content) => {
-            const { prefix, db_name } = config;
-            content = content.replace(`PROJ_PREFIX=myproj\n`, `PROJ_PREFIX=${prefix}\n`);
-            content = content.replace(`MYW_DB_NAME=myproj\n`, `MYW_DB_NAME=${db_name}\n`);
-            return content;
-        });
     }
 }
 
 function fileUpdater(root, config) {
     return (relPath, transform) => {
         const filePath = path.join(root, relPath);
-        let content = fs.readFileSync(filePath, 'utf8');
+        let content;
+        try {
+            content = fs.readFileSync(filePath, 'utf8');
+        } catch (e) {
+            console.warn(`Failed to read file ${filePath}`);
+            return false;
+        }
         content = transform(config, content);
         if (!content) throw new Error('transform returned empty content');
         fs.writeFileSync(filePath, content);
@@ -183,9 +204,7 @@ function fileUpdater(root, config) {
 
 function replaceModuleInjection(content, modules, isDevEnv = false) {
     const isFromInjectorFn = ({ version, devSrc }) => version && !devSrc;
-    const filter1 = isDevEnv
-        ? isFromInjectorFn
-        : ({ version }) => !!version;
+    const filter1 = isDevEnv ? isFromInjectorFn : ({ version }) => !!version;
     const section1 = modules
         .filter(filter1)
         .map(({ name, version }) => `FROM \${CONTAINER_REGISTRY}${name}:${version} as ${name}`)
@@ -195,9 +214,7 @@ function replaceModuleInjection(content, modules, isDevEnv = false) {
         `$1\n${section1}\n$2`
     );
 
-    const filter2 = isDevEnv 
-    ? isFromInjectorFn
-    : ({ devOnly }) => !devOnly;
+    const filter2 = isDevEnv ? isFromInjectorFn : ({ devOnly }) => !devOnly;
     const section2 = modules
         .filter(filter2)
         .map(({ name, version }) =>
