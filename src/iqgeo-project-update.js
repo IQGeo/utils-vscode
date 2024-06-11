32f660f667a97f9dcfb9fb537b4a1efe6966f5e4
Daniel Knights <59598622+Daniel-Knights@users.noreply.github.com>
2024-06-11 20:22:18 +0100

    projectUpdate tool: only notify on log level 1

--- src/iqgeo-project-update.js
+++ src/iqgeo-project-update.js
@@ -1,7 +1,32 @@
+// @ts-check
 import vscode from 'vscode'; // eslint-disable-line
 import util from 'util';
 
 import { pull, update } from 'project-update';
+
+/**
+ * @param {(msg: string) => void} notificationCb
+ * @param {(msg: string) => void} outputCb
+ * @returns {ProgressHandler[keyof ProgressHandler]}
+ */
+function getProgressMethod(notificationCb, outputCb) {
+    return (level, info, moreDetails) => {
+        if (moreDetails) {
+            if (level === 1) {
+                notificationCb(`${info} ([details](command:iqgeo.showOutput))`);
+            }
+
+            outputCb(util.format(info));
+            outputCb(util.format(moreDetails));
+        } else {
+            if (level === 1) {
+                notificationCb(info);
+            }
+
+            outputCb(util.format(info));
+        }
+    };
+}
 
 /**
  * Updates a IQGeo project.
@@ -16,43 +41,14 @@ export class IQGeoProjectUpdate {
             vscode.commands.registerCommand('iqgeo.updateProject', () => this._update()),
             vscode.commands.registerCommand('iqgeo.pullTemplate', () => this._pull())
         );
+
+        /** @type {ProgressHandler} */
+        this._progressHandlers = {
+            log: getProgressMethod(vscode.window.showInformationMessage, outputChannel.info),
+            warn: getProgressMethod(vscode.window.showWarningMessage, outputChannel.warn),
+            error: getProgressMethod(vscode.window.showErrorMessage, outputChannel.error),
+        };
     }
-
-    /** @type {import('project-update').ProgressHandler} */
-    _progressHandlers = {
-        log: (level, info, moreDetails) => {
-            if (moreDetails) {
-                vscode.window.showInformationMessage(
-                    `${info} ([details](command:iqgeo.showOutput))`
-                );
-
-                this.outputChannel.info(util.format(info));
-                this.outputChannel.info(util.format(moreDetails));
-            } else {
-                vscode.window.showInformationMessage(info);
-            }
-        },
-        warn: (level, info, moreDetails) => {
-            if (moreDetails) {
-                vscode.window.showWarningMessage(`${info} ([details](command:iqgeo.showOutput))`);
-
-                this.outputChannel.warn(util.format(info));
-                this.outputChannel.warn(util.format(moreDetails));
-            } else {
-                vscode.window.showWarningMessage(info);
-            }
-        },
-        error: (level, info, moreDetails) => {
-            if (moreDetails) {
-                vscode.window.showErrorMessage(`${info} ([details](command:iqgeo.showOutput))`);
-
-                this.outputChannel.error(util.format(info));
-                this.outputChannel.error(util.format(moreDetails));
-            } else {
-                vscode.window.showErrorMessage(info);
-            }
-        },
-    };
 
     async _update() {
         const workspaceFolders = vscode.workspace.workspaceFolders;
@@ -84,3 +80,7 @@ export class IQGeoProjectUpdate {
         );
     }
 }
+
+/**
+ * @typedef {import('project-update').ProgressHandler} ProgressHandler
+ */
