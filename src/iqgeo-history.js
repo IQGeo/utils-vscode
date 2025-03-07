4bfb1a43da61a073860f011de7207b331840bdb2
Ollie Sims <ollie.sims@iqgeo.com>
2025-03-07 11:25:54 +0000

    Allow option to show most recent methods

--- src/iqgeo-history.js
+++ src/iqgeo-history.js
@@ -10,7 +10,8 @@ const PY_File = {
     scheme: 'file',
     language: 'python',
 };
-const maxHistorySize = 200;
+const maxHistorySize = 100;
+const useNavigationIndex = false;
 
 export class IQGeoHistoryManager {
     constructor(iqgeoVSCode, context) {
@@ -78,8 +79,6 @@ export class IQGeoHistoryManager {
     }
 
     _updateNavigationHistory() {
-        // const start = new Date();
-
         const editor = vscode.window.activeTextEditor;
         if (!editor) return;
         const doc = editor.document;
@@ -114,10 +113,15 @@ export class IQGeoHistoryManager {
                 (item) => item.fileName === fileName && item.name === name
             );
 
-            // console.log('Update History:', fileName, name, index);
-
             if (index !== -1) {
-                this.navigationIndex = index;
+                if (useNavigationIndex) {
+                    this.navigationIndex = index;
+                } else {
+                    // Move the item to the end of the history
+                    const item = this.navigationHistory.splice(index, 1)[0];
+                    this.navigationHistory.push(item);
+                    this.navigationIndex = this.navigationHistory.length - 1;
+                }
             } else {
                 if (this.navigationIndex !== undefined) {
                     this.navigationHistory = this.navigationHistory.slice(
@@ -154,9 +158,6 @@ export class IQGeoHistoryManager {
             this.provider.dispose();
             this.provider = vscode.languages.registerCodeLensProvider([JS_File, PY_File], this);
         }
-
-        // const end = new Date();
-        // console.log('updateNavigationHistory():', end - start, 'ms');
     }
 
     goto(historyItem) {
@@ -168,8 +169,6 @@ export class IQGeoHistoryManager {
         vscode.window.showTextDocument(uri, {
             selection,
             viewColumn: historyItem.viewColumn,
-            // preview,
-            // preserveFocus,
         });
     }
 
@@ -181,14 +180,12 @@ export class IQGeoHistoryManager {
 
         let pos = editor.selection.active;
         if (doc.isDirty) {
-            // Position the actions before the current function whilst editing to avoid to much movement of the actions.
+            // Position the actions before the current function whilst editing to avoid too much movement of the actions.
             const currentItem = this.navigationHistory[this.navigationIndex];
             pos = new vscode.Position(currentItem.range[0], 0);
         }
 
         const range = new vscode.Range(pos, pos);
-
-        // console.log('provideCodeLenses', doc.fileName, pos.line);
 
         const navigationLenses = [];
 
@@ -279,21 +276,21 @@ export class IQGeoHistoryManager {
                     secondLine = true;
                 }
 
-                // const classData = this.iqgeoVSCode.getClassData(currentText);
-                // if (classData) {
-                //     const searchCommand = vscode.Uri.parse(
-                //         `command:iqgeo.searchSymbols?${encodeURIComponent(
-                //             JSON.stringify([{ query: `^${currentText}$.` }])
-                //         )}`
-                //     );
-                //     if (secondLine) {
-                //         hoverString += ` | `;
-                //     } else {
-                //         hoverString += `${lineSeparator}`;
-                //         secondLine = true;
-                //     }
-                //     hoverString += `$(list-unordered)\u2002[Search Class](${searchCommand} "Search class methods")`;
-                // }
+                const classData = this.iqgeoVSCode.getClassData(currentText, doc.languageId);
+                if (classData) {
+                    const searchCommand = vscode.Uri.parse(
+                        `command:iqgeo.searchSymbols?${encodeURIComponent(
+                            JSON.stringify([{ query: `^${currentText}$.` }])
+                        )}`
+                    );
+                    if (secondLine) {
+                        hoverString += ` | `;
+                    } else {
+                        hoverString += `${lineSeparator}`;
+                        secondLine = true;
+                    }
+                    hoverString += `$(list-unordered)\u2002[Search Class](${searchCommand} "Search class methods")`;
+                }
 
                 if (inSelection || inCurrentWord) {
                     const gotoCommand = vscode.Uri.parse('command:iqgeo.goToDefinition');
@@ -309,7 +306,7 @@ export class IQGeoHistoryManager {
 
         const historyString = this._getHistoryHoverString();
         if (historyString) {
-            hoverString += `${lineSeparator}${historyString}`;
+            hoverString += `${lineSeparator}$(history)\u2002${historyString}`;
         }
 
         return hoverString;
@@ -387,26 +384,28 @@ export class IQGeoHistoryManager {
     }
 
     _getHistoryItems() {
-        const inc = this.navigationIndex < 2 ? 3 : 2;
-        const max = Math.min(this.navigationIndex + inc, this.navigationHistory.length - 1);
-        const dec = max > this.navigationIndex ? 4 : 3;
-        const min = Math.max(max - dec, 0);
-
         const items = [];
         const splitPaths = [];
         const titles = [];
+        let min, max;
+
+        if (useNavigationIndex) {
+            const inc = this.navigationIndex < 2 ? 3 : 2;
+            max = Math.min(this.navigationIndex + inc, this.navigationHistory.length - 1);
+            const dec = max > this.navigationIndex ? 4 : 3;
+            min = Math.max(max - dec, 0);
+        } else {
+            max = this.navigationHistory.length - 1;
+            min = Math.max(max - 4, 0);
+        }
 
         for (let index = min; index < max + 1; index++) {
-            if (index !== this.navigationIndex) {
-                const historyItem = this.navigationHistory[index];
-                items.push(historyItem);
-
-                const parts = historyItem.fileName.split(path.sep);
-                parts.push('');
-                parts.push(historyItem.name);
-                parts.reverse();
-                splitPaths.push(parts);
-            }
+            const historyItem = this.navigationHistory[index];
+            const parts = historyItem.fileName.split(path.sep);
+            parts.push('');
+            parts.push(historyItem.name);
+            parts.reverse();
+            splitPaths.push(parts);
         }
 
         const minParts = Math.min(...splitPaths.map((parts) => parts.length));
@@ -430,14 +429,25 @@ export class IQGeoHistoryManager {
         let count = 0;
         for (let index = min; index < max + 1; index++) {
             if (index !== this.navigationIndex) {
+                const historyItem = this.navigationHistory[index];
+                items.push(historyItem);
+
                 const name = getUniqueName(count);
-                const title =
-                    index < this.navigationIndex
-                        ? `${'<'.repeat(this.navigationIndex - index)} ${name}`
-                        : `${name} ${'>'.repeat(index - this.navigationIndex)}`;
+                let title = name;
+                if (useNavigationIndex) {
+                    title =
+                        index < this.navigationIndex
+                            ? `${'<'.repeat(this.navigationIndex - index)} ${name}`
+                            : `${name} ${'>'.repeat(index - this.navigationIndex)}`;
+                }
                 titles.push(title);
-                count++;
             }
+            count++;
+        }
+
+        if (!useNavigationIndex) {
+            items.reverse();
+            titles.reverse();
         }
 
         return { items, titles };
